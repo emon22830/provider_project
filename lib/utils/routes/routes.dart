8ace5e831961f6e3eb6873011a568e5d44f36c9d
emon22830 <mdemon22830@gmail.com>
2024-10-27 21:04:31 +0800

    login,home screen part

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:provider_project/utils/routes/routes_name.dart';
+import 'package:provider_project/view/home_screen.dart';
+import 'package:provider_project/view/login_screen.dart';
+
+class Routes {
+  static Route<dynamic>  generateRoute(RouteSettings settings){
+final argume = settings.arguments;
+    switch(settings.name){
+      case RoutesName.home:
+      return MaterialPageRoute(builder: (BuildContext context) => HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (BuildContext context) => LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_){
+          return Scaffold(
+            body: Center(child: Text('No route defined'),),
+          );
+        });
+    }
+  }
+}
---
+++ lib/utils/routes/routes_name.dart
@@ -0,0 +1,12 @@
+
+
+
+class RoutesName {
+
+  //accounts route name
+  static const String login = 'login_screen';
+
+  //home screen route name
+static const String home = 'home_screen';
+
+}
